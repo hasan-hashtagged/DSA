b8f699533fb93966e3c754c079320d5395718906
hj <hasanj1729@gmail.com>
2018-11-07 18:13:40 +0530

    Add inorder predecessor and Modify return type of find_max as pointer

--- bst/bst.c
+++ bst/bst.c
@@ -36,11 +36,11 @@ void postorder(struct node *p) {
 	postorder(p->right);
 	printf("%d ",p->data);
 }
-int find(int x,struct node *p) {
+struct node *find(int x,struct node *p) {
 	if(p==NULL)
-		return -1;
+		return NULL;
 	else if(p->data==x)
-		return 1;
+		return p;
 	else if(p->data>x)
 		find(x,p->left);
 	else
@@ -53,12 +53,29 @@ int find_min(struct node *p) {
 		p=p->left;
 	return (p->data);
 }
-int find_max(struct node *p) {
+struct node *find_max(struct node *p) {
 	if(p==NULL)
-		return -1;
+		return p;
 	while(p->right)
 		p=p->right;
-	return (p->data);
+	return p;
+}
+struct node *predecessor(int x,struct node *p) {
+	struct node *q=find(x,root);
+	if(q->left)
+		return (find_max(q->left));
+	else {
+		struct node *pre=NULL,*temp=p;
+		while(q!=temp) {
+			if(temp->data < q->data) {
+				pre=temp;
+				temp=temp->right;
+			}
+			else
+				temp=temp->left;
+		}
+		return pre;
+	}
 }
 int main() {
 	char ch;
@@ -86,7 +103,7 @@ int main() {
 		}
 		else if(ch=='f') {
 			scanf("%d",&x);
-			if(find(x,root)==1)
+			if(find(x,root)!=NULL)
 				printf("Found\n");
 			else
 				printf("Not Found\n");
@@ -96,10 +113,25 @@ int main() {
 			printf("%d\n",x);
 		}
 		else if(ch=='M') {
-			x=find_max(root);
-			printf("%d\n",x);
+			struct node *p=find_max(root);
+			if(p)	
+				printf("%d\n",p->data);
+			else
+				printf("-1\n");
 		}
-		else if(ch=='s')
+		else if(ch=='r') {
+			scanf("%d",&x);
+			if(root==NULL || (find(x,root)==NULL))
+				printf("NOT FOUND\n");
+			else {
+				struct node *p=predecessor(x,root);
+				if(p)
+					printf("%d\n",p->data);
+				else
+					printf("NIL\n");
+			}
+		}
+		else if(ch=='z')
 			break;
 	}
 }
